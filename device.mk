7faded9ee64956374038fa4d32255ca2c4f80a62
Gilang Erlianul Awal <144137514+excaliburXD@users.noreply.github.com>
2023-12-31 06:50:56 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -26,9 +26,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Enable Virtual A/B OTA
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
-
 ENABLE_VIRTUAL_AB := true
 AB_OTA_UPDATER := true
 
